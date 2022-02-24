0f9926aab91f99b9d990367d748c76038c94834a
longndt <55429399+longndt@users.noreply.github.com>
2022-02-24 16:17:03 +0700

    demo

---
+++ demoweb123/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        //tạo tài khoản demo cho role Admin
+        $user = new User;
+        $user->setUsername("admin");
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->hasher->hashPassword($user,"123456"));
+        $manager->persist($user);
+
+        //tạo tài khoản demo cho role User
+        $user->setUsername("user");
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->hasher->hashPassword($user,"123456"));
+        $manager->persist($user);
+
+        //tạo tài khoản demo cho role Manager
+        $user->setUsername("manager");
+        $user->setRoles(['ROLE_MANAGER']);
+        $user->setPassword($this->hasher->hashPassword($user,"123456"));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
